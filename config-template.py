67b4632cf0458f95fa8213386af16016a7b06f2b
Nestor <nestor@pop-os.localdomain>
2023-10-08 00:55:05 -0400

    template for the config file

---
+++ config-template.py
@@ -0,0 +1,16 @@
+ids = dict(
+    groupId = '',
+    sourceOrgId = ''
+)
+
+authToken = dict(
+    Authorization = ''
+)
+
+csvFile = dict(
+    fileLocation = './organization-names.csv'
+)
+
+restAPIVersion = dict(
+    restVersion = '2023-09-20'
+)
